d58efa3d828853519b7704e117f94bbdbd66c5c1
jamesgneal <nealjg@gmail.com>
2018-02-22 11:19:00 -0500

    DRYer functions, tweaked points system, randomized enemies layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,37 +4,33 @@ $(document).ready(function () {
     var obiWan = {
         name: "obiWan",
         healthPoints: 120,
-        attackPoints: 15,
-        countPoints: 15,
+        attackPoints: 6,
+        countPoints: 30,
         isHero: false,
-        isOpponent: false,
         cardId: "#obiwan-card",
     };
     var maceWindu = {
         name: "maceWindu",
-        healthPoints: 100,
-        attackPoints: 12,
+        healthPoints: 120,
+        attackPoints: 14,
         countPoints: 6,
         isHero: false,
-        isOpponent: false,
         cardId: "#mace-card",
     };
     var darthVader = {
         name: "darthVader",
         healthPoints: 180,
-        attackPoints: 10,
-        countPoints: 30,
+        attackPoints: 2,
+        countPoints: 20,
         isHero: false,
-        isOpponent: false,
         cardId: "#vader-card",
     };
     var bobaFett = {
         name: "bobaFett",
-        healthPoints: 120,
-        attackPoints: 3,
-        countPoints: 9,
+        healthPoints: 150,
+        attackPoints: 5,
+        countPoints: 16,
         isHero: false,
-        isOpponent: false,
         cardId: "#boba-card",
     };
 
@@ -53,21 +49,52 @@ $(document).ready(function () {
     var oppCounter = 0;
     var opponentsRemaining = (characters.length - 1);
 
-    // set the correct visibilities for the gameboard
-    $("#reset-button").hide();
-    $(".active-game").hide();
-    $(".pre-game").show();
+    // set the correct gameboard visibilities for the start of the game
+    var gameStart = function () {
+        $("#reset-button").hide();
+        $("#attack-button").show();
+        $(".active-game").hide();
+        $(".pre-game").show();
+    };
 
-    // CARD CLICKS BEGIN ==============================================================
+    // Shared gameboard functions
+    var healthClassMaker = function (alignment) {
+        characterSelection = "#" + characters[i].name + "Health";
+        $(characterSelection).addClass(alignment + "HealthDisplay");
+    }
+    var redundantAreaClass = function () {
+        $(characters[i].cardId).removeClass("col-xs-3");
+    }
+    var titleColorChanger = function (alignment) {
+        var titleSelector = "#" + characters[i].name + "Info";
+        $(titleSelector).addClass(alignment);
+    }
+
+    // Function to call after hero is picked - shuffles the characters array to randomize the "available enemies" layout. This prevents user from memorizing successful click-orders 
+    function shuffle() {
+        var j, x, i;
+        for (i = characters.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = characters[i];
+            characters[i] = characters[j];
+            characters[j] = x;
+        }
+    }
+
+    gameStart();
+
+    // CARD CLICKS CONTROLS BEGIN ==============================================================
     $(".character").click(function () {
+
+        // Get the value of the clicked character...
         var $this = this;
+        var clicked = $($this).attr("value");
+
         // CHOOSE HERO BEGIN ============================================================================
         if (heroChosen === false) {
-            // Get the value of the clicked character...
-            var changeHero = $($this).attr("value");
             // ...and find the corresponding object, change their isHero boolean, and pass point values to heroHealth, heroAttack, and attackAdder
             for (i = 0; i < characters.length; i++) {
-                if (characters[i].name === changeHero) {
+                if (characters[i].name === clicked) {
                     characters[i].isHero = true;
                     heroHealth = characters[i].healthPoints;
                     heroAttack = characters[i].attackPoints;
@@ -78,21 +105,24 @@ $(document).ready(function () {
             $(".pre-game").hide();
             $(".active-game").show();
 
-            // Move your selection to the player's "Your Character" div and add the hero health class to the HP , the others to the "Enemies" div,
+            // shuffle the deck so the layout of the "enemies available" area is random each game
+            shuffle();
+
+            // Move your selection to the player's "Your Hero" div and add the hero health class to the HP , the others to the "Enemies" div,
             for (i = 0; i < characters.length; i++) {
                 if (characters[i].isHero === true) {
                     $(characters[i].cardId).appendTo("#user-character");
 
-                    // remove the redundant bootstrap class
-                    $(characters[i].cardId).removeClass("col-xs-3");
+                    // remove the now redundant bootstrap class
+                    redundantAreaClass();
+
+                    // add hero health class to HP
+                    healthClassMaker("hero");
 
                     // add the hero class to the child title div
                     var titleColorChanger = "#" + characters[i].name + "Info"
                     $(titleColorChanger).addClass("hero");
-
-                    // add hero health class to HP
-                    var healthClassMaker = "#" + characters[i].name + "Health";
-                    $(healthClassMaker).addClass("heroHealthDisplay");
+                    // titleColorChanger("hero");
 
                     // ...change the message area
                     $("#attack-message").html("<p>Select an enemy character card to battle</p>");
@@ -100,11 +130,14 @@ $(document).ready(function () {
                 }
                 // select the unclicked cards
                 if (characters[i].isHero !== true) {
+
+                    // move them to the enemies div
                     $(characters[i].cardId).appendTo("#enemies");
 
-                    // add the enemies class to the child title divs
+                    // move the enemies class to the child title divs
                     var titleColorChanger = "#" + characters[i].name + "Info"
                     $(titleColorChanger).addClass("enemies");
+                    // titleColorChanger("enemies");
                 }
             }
 
@@ -115,27 +148,22 @@ $(document).ready(function () {
 
         // CHOOSE OPPONENT BEGIN ==============================================================
         if (heroChosen === true && inBattle === false) {
-            // Get the value of the clicked character...
-            var changeEnemy = $($this).attr("value");
             // ...find the corresponding object...
             for (i = 0; i < characters.length; i++) {
                 // ...ensure the clicked character isn't already the hero...
-                if (characters[i].name === changeEnemy && characters[i].isHero === false) {
-                    // ...change them to the Opponent...
-                    characters[i].isOpponent = true;
+                if (characters[i].name === clicked && characters[i].isHero === false) {
                     // ...move the card to the Opponent area...
                     $(characters[i].cardId).appendTo("#chosen-opponent");
-                    // remove the redundant bootstrap class
-                    $(characters[i].cardId).removeClass("col-xs-3");
+                    // remove the now redundant bootstrap class
+                    redundantAreaClass();
                     // add the opponent class and remove the enemies class to the child title divs
                     var titleColorChanger = "#" + characters[i].name + "Info"
                     $(titleColorChanger).addClass("opponent");
                     $(titleColorChanger).removeClass("enemies");
 
                     // add the health class to their HP display
-                    var healthClassMaker = "#" + characters[i].name + "Health";
-                    $(healthClassMaker).addClass("oppHealthDisplay");
-                    console.log(healthClassMaker);
+                    healthClassMaker("opp");
+
                     // ...pass point values to oppHealth and oppCounter
                     oppHealth = characters[i].healthPoints;
                     oppCounter = characters[i].countPoints;
